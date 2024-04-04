f43ea543f8c1584149cae9fad65924d88c56591b
MUTHURAJ.P <111838220+Muthu0706@users.noreply.github.com>
2024-04-04 13:44:18 +0530

    Delete Capstone4 directory

--- Capstone4/src/main/java/com/csp/dao/InvitationDao.java
+++
@@ -1,8 +0,0 @@
-package com.csp.dao;
-import com.csp.bean.Invitation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvitationDao extends JpaRepository<Invitation, Long> {
-}
--- Capstone4/src/main/java/com/csp/dao/PasswordManagerDao.java
+++
@@ -1,8 +0,0 @@
-package com.csp.dao;
-import com.csp.bean.PasswordManager;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PasswordManagerDao extends JpaRepository<PasswordManager, Long> {
-}
